2133c9785842e5140c1b6f37545d4705bb175500
Bernd Engels <engels@goldenacker.de>
2020-05-14 14:01:32 +0200

    suff

--- resources/views/hallo.blade.php
+++ resources/views/hallo.blade.php
@@ -1,25 +1,33 @@
 @extends('layouts.simple')
 
 @section('content')
-    <h3>Hallo Welt das ist mein Inhalt</h3>
-    <div class="m-4">
-        <form>
-            <!-- erst ab laravel version 7 -->
-            <x-form.input.text name="fullname" value="Paul Panter" :msg="$message" />
-            <x-form.input.email name="email" value="engels@goldenacker.de" />
-            <x-form.input.select name="autoren" value="1" :options="$options" />
-            {{--
-            <!-- bis laravel version <= 6 -->
-            // alles auskommentiert
-            @include('components.form.input.text', [
-                'name' => 'fullname',
-                'value' => 'Paul Meier',
-            ])
-            @include('components.form.input.email', [
-                'name' => 'email',
-                'value' => 'engels@goldenacker.de',
-            ])
-            --}}
-        </form>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Hallo Welt das ist mein Inhalt</div>
+                    <div class="card-body">
+                        <form>
+                            <!-- erst ab laravel version 7 -->
+                            <x-form.input.text name="fullname" value="Paul Panter" :msg="$message" />
+                            <x-form.input.email name="email" value="engels@goldenacker.de" />
+                            <x-form.input.select name="autoren" value="1" :options="$options" />
+                            {{--
+                            <!-- bis laravel version <= 6 -->
+                            // alles auskommentiert
+                            @include('components.form.input.text', [
+                                'name' => 'fullname',
+                                'value' => 'Paul Meier',
+                            ])
+                            @include('components.form.input.email', [
+                                'name' => 'email',
+                                'value' => 'engels@goldenacker.de',
+                            ])
+                            --}}
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
